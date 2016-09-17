4d3c184364ce6c659b638b0f6e765121e821ed22
nekosyakolya <greirex@mail.ru>
2016-09-17 22:52:41 +0300

    изменение имени исходника

--- laba1_1/Debug/test.bat
+++ laba1_1/Debug/test.bat
@@ -1,4 +1,7 @@
-laba1_1.exe ini.txt "hello"
-laba1_1.exe in.txt 
-laba1_1.exe in.txt "hello"
-laba1_1.exe in.txt ""
+laba1_1.exe ini.txt out.txt "hello" hi
+laba1_1.exe in.txt out.txt
+laba1_1.exe in1.txt out1.txt hello hellohello
+laba1_1.exe in.txt out.txt hi ""
+laba1_1.exe in.txt out.txt 1231234 hello
+
+
