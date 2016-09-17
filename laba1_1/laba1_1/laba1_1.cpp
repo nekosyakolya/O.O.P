662b2c12e6b22950d0341f1b4206d4189143b051
nekosyakolya <greirex@mail.ru>
2016-09-17 21:30:00 +0300

    выделение отдельной функции replaceString

--- laba1_1/laba1_1/laba1_1.cpp
+++ laba1_1/laba1_1/laba1_1.cpp
@@ -6,12 +6,24 @@
 
 using namespace std;
 bool IsIdenticalString(const string &searchStr, const string &replaceStr);
+string ReplaceString(const string &strInFile, const string &searchStr, const string &replaceStr);
 
 bool IsIdenticalString(const string &searchStr, const string &replaceStr)
 {
 	bool identicalStrings;
 	(searchStr == replaceStr) ? identicalStrings = true : identicalStrings = false;
 	return identicalStrings;
+}
+
+string ReplaceString(string &strInFile, const string &searchStr, const string &replaceStr)
+{
+	size_t pos = strInFile.find(searchStr);
+	while ((pos != string::npos) && !IsIdenticalString(searchStr, replaceStr))
+	{
+		strInFile.replace(pos, searchStr.length(), replaceStr);
+		pos = strInFile.find(searchStr, pos + replaceStr.length());
+	}
+	return strInFile;
 }
 
 
@@ -31,30 +43,35 @@ int main(int argc, char * argv[])
 		cout << "Failed to open " << argv[1] << " for reading\n";
 		return 1;
 	}
+
 	ofstream output(argv[2]);
+
 	if (!output.is_open())
 	{
 		cout << "Failed to open " << argv[2] << " for writing\n";
 		return 1;
 	}
 
-	string strInFile, searchStr = argv[3], replaceStr = argv[4];
+	string searchStr = argv[3], replaceStr = argv[4];
 
 	if ((searchStr.empty()) ||(replaceStr.empty()))
 	{
 		cout << "Empty line \n";
 		return 1;
 	}
+
+	string strInFile;
 	while (getline(input, strInFile))
 	{
-		size_t pos = strInFile.find(searchStr);
-		while ((pos != string::npos) && !IsIdenticalString(searchStr, replaceStr))
+
+		if (!strInFile.empty())
 		{
-			strInFile.replace(pos, searchStr.length(), replaceStr);
-			pos = strInFile.find(searchStr, pos + replaceStr.length());
+			strInFile = ReplaceString(strInFile, searchStr, replaceStr);
+			output << strInFile;
 		}
-		output << strInFile << "\n";
+		output << "\n";
 	}
+
 	if (!output.flush()) 
 	{
 		cout << "Failed to save data on disk\n";
