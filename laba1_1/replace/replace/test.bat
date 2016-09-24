4eac3eadcbeebab7279c2763b2686e5b418f58fb
nekosyakolya <greirex@mail.ru>
2016-09-24 14:01:21 +0300

    изменены тесты

--- laba1_1/replace/replace/test.bat
+++ laba1_1/replace/replace/test.bat
@@ -19,23 +19,23 @@ fc.exe %TEMP%\empty.txt empty.txt
 if %ERRORLEVEL% EQU 1 goto err
 
 rem check empty line
-%PROGRAM% in1.txt %TEMP%\in1.txt "" "hi"
+%PROGRAM% numTest.txt %TEMP%\numTest.txt "" "hi"
 if %ERRORLEVEL% EQU 0 goto err
-fc.exe %TEMP%\in1.txt in1.txt
+fc.exe %TEMP%\numTest.txt numTest.txt
 if %ERRORLEVEL% EQU 0 goto err
 
-rem check ma mama
-%PROGRAM% in1.txt  %TEMP%\in1.txt "ma" "mama"
+rem check lo lolo
+%PROGRAM% haplology.txt  %TEMP%\haplology.txt "lo" "lolo"
 if %ERRORLEVEL% EQU 1 goto err
-fc.exe %TEMP%\in1.txt in1.txt
+fc.exe %TEMP%\haplology.txt haplology.txt
 if %ERRORLEVEL% EQU 0 goto err
-
 
 rem check 1231234
-%PROGRAM% in2.txt %TEMP%\in2.txt "1231234" "hi"
+%PROGRAM% numTest.txt %TEMP%\numTest.txt "1231234" "perfect"
 if %ERRORLEVEL% EQU 1 goto err
-fc.exe %TEMP%\in2.txt in2.txt
+fc.exe %TEMP%\numTest.txt numTest.txt
 if %ERRORLEVEL% EQU 0 goto err
+
 
 echo Program testing succeeded
 exit 0
