d7fe8cae37080d6a82a33476def59841103c42a7
nekosyakolya <greirex@mail.ru>
2016-09-26 01:45:58 +0300

    изменение тестов

--- laba1_1/replace/replace/test.bat
+++ laba1_1/replace/replace/test.bat
@@ -1,24 +1,24 @@
 set PROGRAM="%~1"
 
-rem check Invalid arguments count
+rem check the number of arguments
 %PROGRAM% in1.txt %TEMP%\in.txt 
 if %ERRORLEVEL% EQU 0 goto err
 fc.exe %TEMP%\in.txt in1.txt 
 if %ERRORLEVEL% EQU 0 goto err
 
-rem check Failed to open
+rem check opening a nonexistent file 
 %PROGRAM% ini.txt %TEMP%\ini.txt "hello" "hi"
 if %ERRORLEVEL% EQU 0 goto err
 fc.exe %TEMP%\ini.txt ini.txt 
 if %ERRORLEVEL% EQU 0 goto err
 
-rem check empty file
+rem check opening an empty file
 %PROGRAM% empty.txt  %TEMP%\empty.txt "hello" "hi"
 if %ERRORLEVEL% EQU 1 goto err1
 fc.exe %TEMP%\empty.txt empty.txt
 if %ERRORLEVEL% EQU 1 goto err
 
-rem check empty line
+rem check an empty string
 %PROGRAM% numTest.txt %TEMP%\numTest.txt "" "hi"
 if %ERRORLEVEL% EQU 0 goto err
 fc.exe %TEMP%\numTest.txt numTest.txt
