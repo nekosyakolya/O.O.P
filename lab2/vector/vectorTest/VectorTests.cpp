4d949889d0add3590930f4d542a6b8b4fbf21dbd
nekosyakolya <greirex@mail.ru>
2016-10-18 22:54:51 +0300

    новые тесты

--- lab2/vector/vectorTest/VectorTests.cpp
+++ lab2/vector/vectorTest/VectorTests.cpp
@@ -34,4 +34,18 @@ BOOST_AUTO_TEST_SUITE(ProcessVectorTest)
 		BOOST_CHECK(VectorsAreEqual(numbers, {0, 0, 0, 0, 0}));
 	}
 
+	BOOST_AUTO_TEST_CASE(max_and_min_are_negative)
+	{
+		vector<double> numbers = {-32, -1, -3, -6.54};
+		ChangeDigits(numbers);
+		BOOST_CHECK(VectorsAreEqual(numbers, {-1, -0.204375, -0.09375, -0.03125}));
+	}
+
+	BOOST_AUTO_TEST_CASE(min_is_negative_and_max_is_positive)
+	{
+		vector<double> numbers = {-32, 3.1, 16};
+		ChangeDigits(numbers);
+		BOOST_CHECK(VectorsAreEqual(numbers, {-8, -1.55, 16}));
+	}
+
 BOOST_AUTO_TEST_SUITE_END()
