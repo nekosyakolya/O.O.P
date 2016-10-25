bef76c606302db087dff964c70fb435ea7f93513
nekosyakolya <greirex@mail.ru>
2016-10-26 00:22:12 +0300

    изменение названий,изменение цикла

--- lab2/GeneratePrimeNumbersSet/GeneratePrimeNumbersSet/GeneratePrimeNumbersSet.cpp
+++ lab2/GeneratePrimeNumbersSet/GeneratePrimeNumbersSet/GeneratePrimeNumbersSet.cpp
@@ -6,30 +6,36 @@
 #include <cmath>
 
 using namespace std;
+const static unsigned MIN_PRIME_NUMBER = 2;
 
-void ScreenNumbers(unsigned n, vector<bool> &prime)
+void ScreenNumbers(unsigned upperLimitNumbers, vector<bool> &isPrimeNumbers)
 {
-	prime[0] = false;
-	prime[1] = false;
-
-	for (unsigned i = 2; i <= sqrt(n); ++i)
+	isPrimeNumbers[0] = false;
+	if (upperLimitNumbers != 0)
 	{
-		if (prime[i])
+		isPrimeNumbers[1] = false;
+	}
+
+	for (unsigned i = MIN_PRIME_NUMBER; i <= sqrt(upperLimitNumbers);
+		        i == MIN_PRIME_NUMBER ? ++i : i += MIN_PRIME_NUMBER)
+	{
+		if (isPrimeNumbers[i])
 		{
-			for (unsigned j = i * i; j <= n; i == 2 ? (j += i) : (j = j + 2 * i))
+			for (unsigned j = i * i; j <= upperLimitNumbers;
+				i == MIN_PRIME_NUMBER ? (j += i) : (j = j + MIN_PRIME_NUMBER * i))
 			{
-				prime[j] = false;
+				isPrimeNumbers[j] = false;
 			}
 		}
 	}
 }
 
-set<unsigned> AddPrimeNumbers(unsigned n, const vector<bool> &prime)
+set<unsigned> AddPrimeNumbers(unsigned upperLimitNumbers, const vector<bool> &isPrimeNumbers)
 {
 	set<unsigned>PrimeNumbersSet;
-	for (unsigned i = 2; i <= n; ++i)
+	for (unsigned i = MIN_PRIME_NUMBER; i <= upperLimitNumbers; ++i)
 	{
-		if (prime[i])
+		if (isPrimeNumbers[i])
 		{
 			PrimeNumbersSet.insert(i);
 		}
@@ -38,28 +44,36 @@ set<unsigned> AddPrimeNumbers(unsigned n, const vector<bool> &prime)
 }
 
 
-set<unsigned> GeneratePrimeNumbersSet(unsigned n)
+set<unsigned> GeneratePrimeNumbersSet(unsigned upperLimitNumbers)
 {
 
-	vector<bool> prime(n + 1, true);
-	ScreenNumbers(n, prime);
+	vector<bool> isPrimeNumbers(upperLimitNumbers + 1, true);
+	ScreenNumbers(upperLimitNumbers, isPrimeNumbers);
 
 	set<unsigned>PrimeNumbersSet;
 
-	return PrimeNumbersSet = AddPrimeNumbers(n, prime);
+	return PrimeNumbersSet = AddPrimeNumbers(upperLimitNumbers, isPrimeNumbers);
+}
+
+bool IsCorrectNumber(unsigned upperLimitNumbers)
+{
+	return (upperLimitNumbers <= 100000000);
 }
 
 int main()
 {
-	unsigned n;
-	cin >> n;
+	unsigned upperLimitNumbers;
+	cin >> upperLimitNumbers;
+	if (!IsCorrectNumber(upperLimitNumbers))
+	{
+		cout << "incorrect number\n";
+		return EXIT_FAILURE;
+	}
 
-	set<unsigned>PrimeNumbersSet = GeneratePrimeNumbersSet(n);
+	set<unsigned>PrimeNumbersSet = GeneratePrimeNumbersSet(upperLimitNumbers);
 
 	copy(PrimeNumbersSet.begin(), PrimeNumbersSet.end(), ostream_iterator<double>(cout, " "));
 
-
-	//cout << PrimeNumbersSet.size() << endl;
 	return EXIT_SUCCESS;
 }
 
