1742eca25d86d1e95c48c89e4498102700518523
nekosyakolya <greirex@mail.ru>
2016-10-26 09:31:56 +0300

    изменение названий тестов

--- lab2/GeneratePrimeNumbersSet/Test/Test.cpp
+++ lab2/GeneratePrimeNumbersSet/Test/Test.cpp
@@ -3,19 +3,19 @@
 
 BOOST_AUTO_TEST_SUITE(TestGeneratePrimeNumbersSet)
 
-	BOOST_AUTO_TEST_CASE(creates_a_set_from_the_upper_bound_0)
+	BOOST_AUTO_TEST_CASE(creates_a_set_when_the_upper_bound_numbers_0)
 	{
 		std::set<unsigned> referenceSet = {};
 		BOOST_CHECK(GeneratePrimeNumbersSet(0) == referenceSet);
 	}
 
-	BOOST_AUTO_TEST_CASE(creates_a_set_from_the_upper_bound_2)
+	BOOST_AUTO_TEST_CASE(creates_a_set_when_the_upper_bound_numbers_2)
 	{
 		std::set<unsigned> referenceSet = { 2 };
 		BOOST_CHECK(GeneratePrimeNumbersSet(2) == referenceSet);
 	}
 
-	BOOST_AUTO_TEST_CASE(creates_a_set_from_the_upper_bound_100)
+	BOOST_AUTO_TEST_CASE(creates_a_set_when_the_upper_bound_numbers_100)
 	{
 		std::set<unsigned> referenceSet = { 2, 3, 5, 7, 11, 13, 17, 19, 23,
 			29, 31, 37, 41, 43, 47, 53, 59,
@@ -23,7 +23,7 @@ BOOST_AUTO_TEST_SUITE(TestGeneratePrimeNumbersSet)
 		BOOST_CHECK(GeneratePrimeNumbersSet(100) == referenceSet);
 	}
 
-	BOOST_AUTO_TEST_CASE(upper_bound_is_equal_to_the_maximum_value)
+	BOOST_AUTO_TEST_CASE(when_the_upper_bound_numbers_is_equal_to_the_maximum_value)
 	{
 		BOOST_CHECK(GeneratePrimeNumbersSet(100000000).size() == 5761455);
 	}
