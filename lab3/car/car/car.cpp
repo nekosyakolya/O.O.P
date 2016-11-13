8669c01106ab8a1259853e70a4f102e49b625855
nekosyakolya <greirex@mail.ru>
2016-11-13 20:36:23 +0300

    добавление новых методов

--- lab3/car/car/car.cpp
+++ lab3/car/car/car.cpp
@@ -4,7 +4,17 @@
 #include "stdafx.h"
 #include "car.h"
 
+bool IsValidGear(int gear)
+{
+	return (gear >= MIN_GEAR) && (gear <= MAX_GEAR);
+}
 
+bool IsValidSpeed(const Gear &gear, int currentSpeed)
+{
+	auto it = speedRange.find(gear);
+	auto range = it->second;
+	return ((currentSpeed >= range.first) && (currentSpeed <= range.second));
+}
 
 
 bool CCar::IsTurnedOn() const
@@ -14,11 +24,68 @@ bool CCar::IsTurnedOn() const
 
 bool CCar::TurnOnEngine()
 {
+	return (m_isOn) ? false : m_isOn = true;
+}
+
+bool CCar::TurnOffEngine()
+{
+	return (m_isOn && m_currentGear == Gear::NEUTRAL_GEAR && m_currentSpeed == 0) ? !(m_isOn = false) : false;
+}
+
+bool CCar::SetGear(int gear)
+{
+	if (IsValidGear(gear) && ((m_isOn && IsValidSpeed(static_cast<Gear>(gear), m_currentSpeed)) || (!m_isOn && m_currentSpeed == 0)))
+	{
+		if (((m_currentGear == Gear::FIRST_GEAR || m_currentGear == Gear::NEUTRAL_GEAR || m_currentGear == Gear::REVERSE_GEAR)  && m_currentSpeed == 0) ||
+			 (m_currentGear != Gear::REVERSE_GEAR) && (static_cast<Gear>(gear) != Gear::REVERSE_GEAR))
+		{
+			m_currentGear = static_cast<Gear>(gear);
+			return true;
+		}
+	}
 	return false;
 }
 
-CCar::CCar()
+Direction CCar::SetDirection(int speed)
 {
+	Direction directionCar;
+	if (speed == MIN_SPEED)
+	{
+		directionCar = Direction::STAY;
+	}
+	else if (m_currentGear != Gear::REVERSE_GEAR)
+	{
+		directionCar = Direction::FORWARD;
+	}
+	else
+	{
+		directionCar = Direction::BACKWARD;
+	}
+	return directionCar;
+}
+
+bool CCar::SetSpeed(int speed)
+{
+	if (speed >= MIN_SPEED && speed <= MAX_SPEED)
+	{
+		if (IsValidSpeed(m_currentGear, speed) && (m_currentGear != Gear::NEUTRAL_GEAR || (m_currentGear == Gear::NEUTRAL_GEAR && speed == 0)))
+		{
+			m_currentSpeed = speed;
+			m_direction = SetDirection(m_currentSpeed);
+			return true;
+		}
+	}
+	return false;
+}
+
+int CCar::GetSpeed() const
+{
+	return m_currentSpeed;
+}
+
+int CCar::GetDirection() const
+{
+	return static_cast<int>(m_direction);
 }
 
 
--- lab3/car/carTest/carTest.cpp
+++ lab3/car/carTest/carTest.cpp
@@ -14,12 +14,131 @@ struct CarSetFixture
 BOOST_FIXTURE_TEST_SUITE(CarSet, CarSetFixture)
 	BOOST_AUTO_TEST_CASE(is_turned_off_by_default)
 	{
-		BOOST_CHECK(!car.IsTurnedOn());
+	    BOOST_CHECK(!car.IsTurnedOn());
+	}
+	BOOST_AUTO_TEST_CASE(can_be_turned_on)
+	{
+		BOOST_CHECK(car.TurnOnEngine());
+		BOOST_CHECK(car.IsTurnedOn());
 	}
 
-	BOOST_AUTO_TEST_CASE(can_turned_on_car)
+	BOOST_AUTO_TEST_CASE(speed_is_0_by_default)
 	{
-		BOOST_CHECK(!car.IsTurnedOn());
-		BOOST_CHECK(car.TurnOnEngine());
+		BOOST_CHECK_EQUAL(car.GetSpeed(), 0);
 	}
+	struct when_turned_on_ : CarSetFixture
+	{
+		when_turned_on_()
+		{
+			car.TurnOnEngine();
+		}
+	};
+	BOOST_FIXTURE_TEST_SUITE(when_turned_on, when_turned_on_)
+
+		BOOST_AUTO_TEST_CASE(can_be_turned_off)
+		{
+			BOOST_CHECK(car.TurnOffEngine());
+		}
+
+		BOOST_AUTO_TEST_CASE(can_be_turned_revers)
+		{
+			car.SetGear(-1);
+			BOOST_CHECK(car.SetGear(1));
+		}
+
+		BOOST_AUTO_TEST_CASE(can_be_turned_neitral)
+		{
+			BOOST_CHECK(car.SetGear(0));
+		}
+
+		BOOST_AUTO_TEST_CASE(cant_be_turned_5)
+		{
+			BOOST_CHECK(!car.SetGear(5));
+		}
+
+		BOOST_AUTO_TEST_CASE(cant_be_turned_4)
+		{
+			car.SetGear(-1);
+			BOOST_CHECK(!car.SetGear(4));
+		}
+
+		BOOST_AUTO_TEST_CASE(cant_set_gear_out_of_range)
+		{
+			BOOST_CHECK(!car.SetGear(10));
+		}
+
+		BOOST_AUTO_TEST_CASE(cant_set_rear_gear_from_other_gears)
+		{
+			car.SetGear(1);
+			car.SetSpeed(30);
+			car.SetGear(2);
+			BOOST_CHECK(!car.SetGear(-1));
+		}
+		BOOST_AUTO_TEST_CASE(cant_set_speed_out_of_gear_speed_range)
+		{
+			car.SetGear(1);
+			BOOST_CHECK(!car.SetSpeed(150));
+		}
+		BOOST_AUTO_TEST_CASE(cant_set_first_gear_after_rear_gear_and_non_zero_speed)
+		{
+			car.SetGear(-1);
+			car.SetSpeed(10);
+			BOOST_CHECK(!car.SetGear(1));
+		}
+
+		BOOST_AUTO_TEST_CASE(cant_accelerate_at_neutral_gear)
+		{
+			car.SetGear(1);
+			car.SetSpeed(10);
+			car.SetGear(0);
+			BOOST_CHECK(!car.SetSpeed(15));
+		}
+
+		BOOST_AUTO_TEST_CASE(forward_direction)
+		{
+			car.SetGear(1);
+			car.SetSpeed(10);
+			car.SetGear(0);
+			BOOST_CHECK(car.GetDirection() == 1);
+		}
+
+		BOOST_AUTO_TEST_CASE(backward_direction)
+		{
+			car.SetGear(-1);
+			car.SetSpeed(10);
+			BOOST_CHECK(car.GetDirection() == -1);
+		}
+
+		BOOST_AUTO_TEST_CASE(direction_isnt_stay)
+		{
+			car.SetGear(1);
+			car.SetSpeed(10);
+			car.SetGear(0);
+			BOOST_CHECK(!car.GetDirection() == 0);
+		}
+
+		BOOST_AUTO_TEST_SUITE(Turn_off_function)
+
+			BOOST_AUTO_TEST_CASE(engine_can_be_turned_off_at_neutral_gear_and_with_zero_speed)
+			{
+				BOOST_CHECK(car.TurnOffEngine());
+			}
+
+			BOOST_AUTO_TEST_CASE(engine_cant_be_turned_off_at_neutral_gear_and_with_non_zero_speed)
+			{
+				car.SetGear(1);
+				car.SetSpeed(20);
+				car.SetGear(0);
+				BOOST_CHECK(!car.TurnOffEngine());
+			}
+
+			BOOST_AUTO_TEST_CASE(engine_cant_be_turned_off_at_non_neutral_gear)
+			{
+				car.SetGear(1);
+				BOOST_CHECK(!car.TurnOffEngine());
+			}
+
+		BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
