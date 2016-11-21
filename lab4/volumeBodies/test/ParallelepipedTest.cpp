d3643294699e99758d1c19f32d009b45bd1cecd6
nekosyakolya <greirex@mail.ru>
2016-11-22 01:50:04 +0300

    тесты к классу CParallelepiped

--- lab4/volumeBodies/test/ParallelepipedTest.cpp
+++ lab4/volumeBodies/test/ParallelepipedTest.cpp
@@ -1,3 +1,4 @@
+// test.cpp: определяет точку входа для консольного приложения.
 //
 
 #include "stdafx.h"
@@ -17,35 +18,44 @@ struct Parallelepiped_
 		: parallelepiped(expectedDensity, expectedWidth, expectedHeight, expectedDepth)
 	{}
 };
+// Параллелепипед
 BOOST_FIXTURE_TEST_SUITE(Parallelepiped, Parallelepiped_)
+// является объемным телом
 	BOOST_AUTO_TEST_CASE(is_a_body)
 	{
 		BOOST_CHECK(static_cast<const CBody*>(&parallelepiped));
 	}
+	// имеет глубину
 	BOOST_AUTO_TEST_CASE(has_a_depth)
 	{
 		BOOST_CHECK_EQUAL(parallelepiped.GetDepth(), expectedDepth);
 	}
+	// имеет ширину
 	BOOST_AUTO_TEST_CASE(has_a_width)
 	{
 		BOOST_CHECK_EQUAL(parallelepiped.GetWidth(), expectedWidth);
 	}
+	// имеет высоту
 	BOOST_AUTO_TEST_CASE(has_a_height)
 	{
 		BOOST_CHECK_EQUAL(parallelepiped.GetHeight(), expectedHeight);
 	}
+	// имеет плотность
 	BOOST_AUTO_TEST_CASE(has_a_density)
 	{
 		BOOST_CHECK_EQUAL(static_cast<const CBody &>(parallelepiped).GetDensity(), expectedDensity);
 	}
+	// имеет объем
 	BOOST_AUTO_TEST_CASE(has_a_volume)
 	{
 		BOOST_CHECK_CLOSE_FRACTION(static_cast<const CBody &>(parallelepiped).GetVolume(), expectedVolume, 1e-7);
 	}
+	// имеет массу
 	BOOST_AUTO_TEST_CASE(has_a_mass)
 	{
 		BOOST_CHECK_CLOSE_FRACTION(static_cast<const CBody &>(parallelepiped).GetMass(), expectedVolume * expectedDensity, 1e-7);
 	}
+	// имеет строковое представление
 	BOOST_AUTO_TEST_CASE(can_be_converted_to_string)
 	{
 		const auto expectedString = R"(parallelepiped:
