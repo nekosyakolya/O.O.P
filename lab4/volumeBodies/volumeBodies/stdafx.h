b814942d711888f7cd408468712814db199b92d2
nekosyakolya <greirex@mail.ru>
2016-11-23 17:08:35 +0300

    добавлен контроллер и соответствующие тесты

--- lab4/volumeBodies/volumeBodies/stdafx.h
+++ lab4/volumeBodies/volumeBodies/stdafx.h
@@ -11,8 +11,14 @@
 #include <tchar.h>
 
 #include <sstream>
-
+#include <iostream>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <map>
+#include <vector>
+#include <memory>
+
+#include <iterator>
+#include <algorithm>
 
 // TODO: Установите здесь ссылки на дополнительные заголовки, требующиеся для программы
